3ac7a769587f6bf7655e1f3ed589d4a293c916c3
Craig Atkinson <atk.craig@gmail.com>
2022-08-19 08:45:14 -0500

    Initial commit

---
+++ src/main/kotlin/com/atkinsondev/opentelemetry/OpenTelemetryPlugin.kt
@@ -0,0 +1,10 @@
+package com.atkinsondev.opentelemetry
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class OpenTelemetryPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        TODO("Not yet implemented")
+    }
+}
