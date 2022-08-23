ff77fe27a3dc8f982b5fcb9083a32816bd3706ef
Craig Atkinson <atk.craig@gmail.com>
2022-08-23 18:55:45 -0500

    Don't use a global instance

--- src/test/kotlin/com/atkinsondev/opentelemetry/build/OpenTelemetryBuildPluginTest.kt
+++ src/test/kotlin/com/atkinsondev/opentelemetry/build/OpenTelemetryBuildPluginTest.kt
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import strikt.assertions.isGreaterThanOrEqualTo
 import strikt.assertions.isNotNull
 import java.io.File
 import java.nio.file.Path
@@ -21,7 +22,7 @@ import kotlin.io.path.createDirectories
 class OpenTelemetryBuildPluginTest {
 
     @Test
-    fun `should send data to otel`(wmRuntimeInfo: WireMockRuntimeInfo, @TempDir projectRootDirPath: Path) {
+    fun `should send data to OpenTelemetry with HTTP`(wmRuntimeInfo: WireMockRuntimeInfo, @TempDir projectRootDirPath: Path) {
         val wiremockBaseUrl = wmRuntimeInfo.httpBaseUrl
 
         val buildFileContents = """
@@ -77,7 +78,6 @@ class OpenTelemetryBuildPluginTest {
             .withProjectDir(projectRootDirPath.toFile())
             .withArguments("test", "--info", "--stacktrace")
             .withPluginClasspath()
-            .withDebug(true)
             .build()
 
         expectThat(buildResult.task(":test")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
@@ -98,6 +98,74 @@ class OpenTelemetryBuildPluginTest {
 
             val testSpanName = ":test"
             expectThat(otelRequestBodies.find { it.contains(testSpanName) }).isNotNull()
+        }
+    }
+
+    @Test
+    fun `should send data to OpenTelemetry with GRPC`(wmRuntimeInfo: WireMockRuntimeInfo, @TempDir projectRootDirPath: Path) {
+        val wiremockBaseUrl = wmRuntimeInfo.httpBaseUrl
+
+        val buildFileContents = """
+            plugins {
+                id "com.atkinsondev.opentelemetry-build"
+                id "org.jetbrains.kotlin.jvm" version "1.7.10"
+            }
+            
+            repositories {
+                mavenCentral()
+            }
+            
+            dependencies {
+                testImplementation(platform("org.junit:junit-bom:5.9.0"))
+                testImplementation("org.junit.jupiter:junit-jupiter")
+            }
+            
+            test {
+                useJUnitPlatform()
+            }
+            
+            openTelemetryBuild {
+                endpoint = '$wiremockBaseUrl/otel'
+                exporterMode = com.atkinsondev.opentelemetry.build.OpenTelemetryExporterMode.GRPC
+            }
+        """.trimIndent()
+
+        File(projectRootDirPath.toFile(), "build.gradle").writeText(buildFileContents)
+
+        val srcPath = Paths.get(projectRootDirPath.absolutePathString(), "src/main/kotlin").createDirectories()
+        val testPath = Paths.get(projectRootDirPath.absolutePathString(), "src/test/kotlin").createDirectories()
+
+        val sourceFileContents = """
+            fun foo() = "bar"
+        """.trimIndent()
+        File(srcPath.toFile(), "foo.kt").writeText(sourceFileContents)
+
+        val testFileContents = """
+            import org.junit.jupiter.api.Test
+            
+            class FooTest {
+                @Test
+                fun `foo should return bar`() {
+                    assert(foo() == "bar")
+                }
+            }
+        """.trimIndent()
+        File(testPath.toFile(), "FooTest.kt").writeText(testFileContents)
+
+        stubFor(post("/opentelemetry.proto.collector.trace.v1.TraceService/Export").willReturn(ok()))
+
+        val buildResult = GradleRunner.create()
+            .withProjectDir(projectRootDirPath.toFile())
+            .withArguments("test", "--info", "--stacktrace")
+            .withPluginClasspath()
+            .build()
+
+        expectThat(buildResult.task(":test")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+
+        await().untilAsserted {
+            val otelRequests = findAll(postRequestedFor(urlEqualTo("/opentelemetry.proto.collector.trace.v1.TraceService/Export")))
+
+            expectThat(otelRequests.size).isGreaterThanOrEqualTo(1)
         }
     }
 }
