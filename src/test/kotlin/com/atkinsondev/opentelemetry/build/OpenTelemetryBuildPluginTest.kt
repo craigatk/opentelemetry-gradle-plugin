a4ef17519acf7afdd62bfab955f1c962e3476d4e
Craig Atkinson <atk.craig@gmail.com>
2022-08-23 19:06:30 -0500

    Refactoring out some commanalities in the test

--- src/test/kotlin/com/atkinsondev/opentelemetry/build/OpenTelemetryBuildPluginTest.kt
+++ src/test/kotlin/com/atkinsondev/opentelemetry/build/OpenTelemetryBuildPluginTest.kt
@@ -14,9 +14,6 @@ import strikt.assertions.isGreaterThanOrEqualTo
 import strikt.assertions.isNotNull
 import java.io.File
 import java.nio.file.Path
-import java.nio.file.Paths
-import kotlin.io.path.absolutePathString
-import kotlin.io.path.createDirectories
 
 @WireMockTest
 class OpenTelemetryBuildPluginTest {
@@ -59,25 +56,8 @@ class OpenTelemetryBuildPluginTest {
 
         File(projectRootDirPath.toFile(), "build.gradle").writeText(buildFileContents)
 
-        val srcPath = Paths.get(projectRootDirPath.absolutePathString(), "src/main/kotlin").createDirectories()
-        val testPath = Paths.get(projectRootDirPath.absolutePathString(), "src/test/kotlin").createDirectories()
-
-        val sourceFileContents = """
-            fun foo() = "bar"
-        """.trimIndent()
-        File(srcPath.toFile(), "foo.kt").writeText(sourceFileContents)
-
-        val testFileContents = """
-            import org.junit.jupiter.api.Test
-            
-            class FooTest {
-                @Test
-                fun `foo should return bar`() {
-                    assert(foo() == "bar")
-                }
-            }
-        """.trimIndent()
-        File(testPath.toFile(), "FooTest.kt").writeText(testFileContents)
+        createSrcDirectoryAndClassFile(projectRootDirPath)
+        createTestDirectoryAndClassFile(projectRootDirPath)
 
         stubFor(post("/otel").willReturn(ok()))
 
@@ -146,25 +126,8 @@ class OpenTelemetryBuildPluginTest {
 
         File(projectRootDirPath.toFile(), "build.gradle").writeText(buildFileContents)
 
-        val srcPath = Paths.get(projectRootDirPath.absolutePathString(), "src/main/kotlin").createDirectories()
-        val testPath = Paths.get(projectRootDirPath.absolutePathString(), "src/test/kotlin").createDirectories()
-
-        val sourceFileContents = """
-            fun foo() = "bar"
-        """.trimIndent()
-        File(srcPath.toFile(), "foo.kt").writeText(sourceFileContents)
-
-        val testFileContents = """
-            import org.junit.jupiter.api.Test
-            
-            class FooTest {
-                @Test
-                fun `foo should return bar`() {
-                    assert(foo() == "bar")
-                }
-            }
-        """.trimIndent()
-        File(testPath.toFile(), "FooTest.kt").writeText(testFileContents)
+        createSrcDirectoryAndClassFile(projectRootDirPath)
+        createTestDirectoryAndClassFile(projectRootDirPath)
 
         stubFor(post("/opentelemetry.proto.collector.trace.v1.TraceService/Export").willReturn(ok()))
 
